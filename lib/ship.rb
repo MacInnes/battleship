30f97f5d1448fe7275d1d495faa47309c3187a51
Andrew Mockett <califormula@gmail.com>
2018-05-17 21:23:27 -0600

    adds ship class

---
+++ lib/ship.rb
@@ -0,0 +1,21 @@
+require './lib/tile'
+require './lib/board'
+
+class Ship
+  attr_reader :length,
+              :hits
+
+  def initialize(length = 2)
+    @length = length
+    @hits = 0
+  end
+
+  def hit
+    @hits += 1
+  end
+
+  def sunk?
+    return @hits == length 
+  end
+
+end
---
+++ test/ship_test.rb
@@ -0,0 +1,57 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/board'
+require './lib/tile'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+
+  def test_exists
+    ship = Ship.new
+
+    assert_instance_of Ship, ship
+  end
+
+  def test_length_can_be_assigned_as_argument
+    ship_2 = Ship.new
+    ship_3 = Ship.new(3)
+
+    assert_equal 2, ship_2.length
+    assert_equal 3, ship_3.length
+  end
+
+  def test_can_be_hit
+    ship = Ship.new
+    ship.hit
+
+    assert_equal 1, ship.hits
+  end
+
+  def test_can_be_sunk
+    ship = Ship.new
+    ship.hit
+
+    refute ship.sunk?
+
+    ship.hit
+
+    assert ship.sunk?
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
