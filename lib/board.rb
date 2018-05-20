9c247d1dd1a9e45c962377784d38be63fdd08744
Andrew Mockett <califormula@gmail.com>
2018-05-20 13:44:01 -0600

    initial ship placement

--- lib/board.rb
+++ lib/board.rb
@@ -25,9 +25,41 @@ class Board
       "D" => 3
     }
 
-    row_1 = pos_1[0]
-    column_1 = pos_1[1]
-    
+    row_1 = board_key[pos_1.chars[0]]
+    column_1 = pos_1.chars[1].to_i - 1
+
+    row_2 = board_key[pos_2.chars[0]]
+    column_2 = pos_2.chars[1].to_i - 1
+
+    if pos_3
+      row_3 = board_key[pos_3.chars[0]]
+      column_3 = pos_3.chars[1].to_i - 1
+      if verify_placement(row_1, column_1, row_2, column_2, row_3, column_3)      
+        @board[row_1][column_1].ship = true
+        @board[row_2][column_2].ship = true
+        @board[row_3][column_3].ship = true
+        return true
+      end
+    elsif verify_placement(row_1, column_1, row_2, column_2)      
+        @board[row_1][column_1].ship = true
+        @board[row_2][column_2].ship = true
+        return true
+    else
+      return false #????  everything else returns side effects, this doesn't make sense
+    end
+
   end
+
+  def verify_placement(row_1, column_1, row_2, column_2, row_3 = nil, column_3 = nil)
+    if @board[row_1][column_1].ship? || @board[row_2][column_2].ship?
+      return false
+    elsif row_3
+      return !@board[row_1][column_1].ship?
+    end
+    return true
+  end
+
+
+
 
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -19,17 +19,37 @@ class BoardTest < Minitest::Test
     assert_equal Tile, board.board[0][0].class
   end
 
-  # def test_board_can_have_ships_placed_on_it
-  #   board = Board.construct
-  #   ship = Ship.new
+  def test_board_can_have_ships_placed_on_it
+    board = Board.construct
+    ship = Ship.new
 
 
-  #   board.place(ship, "A1", "A2") # refactor to accept user/cpu argument?
+    board.place(ship, "A1", "A2") # refactor to accept user/cpu argument?
 
-  #   assert board.board[0][0].ship?
-  #   assert board.board[0][1].ship?
-  #   refute board.board[1][1].ship?
-  # end
+    assert board.board[0][0].ship?
+    assert board.board[0][1].ship?
+    refute board.board[1][1].ship?
+
+    ship_3 = Ship.new(3)
+
+    board.place(ship_3, "B1", "B2", "B3")
+
+    assert board.board[1][0].ship?
+    assert board.board[1][1].ship?
+    assert board.board[1][2].ship?
+    refute board.board[1][3].ship?
+  end
+
+  def test_board_will_fail_invalidly_placed_ships
+    board = Board.construct
+    ship_1 = Ship.new
+    ship_2 = Ship.new
+
+    board.place(ship_1, "A1", "A2")
+    board.place(ship_2, "A1", "A2")
+    
+
+  end
 
 
 end
