ec4b3321f81404cbbdd5704ac8827cbc60b0e685
Andrew Mockett <califormula@gmail.com>
2018-05-21 12:50:36 -0600

    stages two new files

---
+++ test/computer_moves_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './test/test_helper'
+require './lib/board'
+require './lib/tile'
+require './lib/ship'
+require './lib/computer_placement'
