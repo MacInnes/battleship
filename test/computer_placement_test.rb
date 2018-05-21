fa2825a16b903ef46872ee5aa8622c75bbd88363
Andrew Mockett <califormula@gmail.com>
2018-05-21 09:48:05 -0600

    cleanup commit

---
+++ test/computer_placement_test.rb
@@ -0,0 +1,37 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/board"
+require "./lib/tile"
+require "./lib/ship"
+require "./lib/computer_placement"
+
+
+class TestComputerPlacement < Minitest::Test
+  def test_exists
+    computer = ComputerPlacement.new
+
+    assert_instance_of ComputerPlacement, computer
+  end
+
+  def test_selects_two_valid_points
+    board = Board.construct
+
+    computer_placement = ComputerPlacement.new
+    cpu_ship = Ship.new
+
+    computer_placement.place_ship(cpu_ship, board)
+
+    actual = board.board.map do |row|
+      row.select do |tile|
+        tile.ship_name == cpu_ship
+      end
+    end.flatten.length
+
+    assert_equal 2, actual
+  end
+
+  def test_selects_three_valid_points
+
+  end
+
+end
